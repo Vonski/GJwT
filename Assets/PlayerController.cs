e9c252a3dd352e3017252a749aeb4966f4b2e1ab
Vonski <daniellodelapedro@interia.pl>
2016-10-18 16:09:08 +0200

    Basic basic jump

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,400f));
+
+	}
+}
