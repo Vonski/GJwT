7060f945422ac9f2cedc2430ea46adba5b78bcc0
DevRaptor <krzysztoftaperek@gmail.com>
2016-10-19 00:11:31 +0200

    Added shooting to birds in Mister_three

--- Assets/Raptor.cs
+++ Assets/Raptor.cs
@@ -17,25 +17,22 @@ public class Raptor : MonoBehaviour {
 		case Actions.Mister1:
 			GetComponent<Mister_one>().enabled = true;
 			break;
-		case Actions.Kids2:
+		case Actions.Mister2:
 			GetComponent<Mister_one>().enabled = false;
 			GetComponent<Mister_two>().enabled = true;
 			break;
-		case Actions.Mister2:
+		case Actions.Mister3:
 			GetComponent<Mister_two>().enabled = false;
 			GetComponent<Mister_three>().enabled = true;
 			break;
-		case Actions.Mister3:
+		case Actions.Mister4:
 			GetComponent<Mister_three>().enabled = false;
 			GetComponent<Mister_four>().enabled = true;
 			break;
-		case Actions.Mister4:
+		/*case Actions.Mister5:
 			GetComponent<Mister_four>().enabled = false;
 		
-			break;
+			break;*/
         }
-
-        if (Input.GetMouseButtonDown(0))
-            currentAction++;
 	}
 }
