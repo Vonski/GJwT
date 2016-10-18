f418f56bb0669790221e67317fcf0998a9393494
Vonski <daniellodelapedro@interia.pl>
2016-10-18 18:48:33 +0200

    Easy mini games changing

---
+++ Assets/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    private int currentAction;
+    public Transform controller;
+    public Transform p1;
+    public Transform p2;
+    public Transform p3;
+
+    // Update is called once per frame
+    void Update () {
+
+        currentAction = controller.GetComponent<ScenarioController>().currentAction;
+
+        switch (currentAction)
+        {
+            case 1:
+                transform.position = p1.transform.position + Vector3.back*10;
+                break;
+            case 6:
+                transform.position = p2.transform.position + Vector3.back * 10;
+                break;
+            case 11:
+                transform.position = p3.transform.position + Vector3.back * 10;
+                break;
+        }
+    }
+}
--- Assets/ScenarioController.cs
+++ Assets/ScenarioController.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class ScenarioController : MonoBehaviour {
 
+    public int currentAction;
+
 	// Use this for initialization
 	void Start () {
-	
+        currentAction = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    switch (currentAction)
+        {
+            case 1:
+                break;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+            currentAction++;
 	}
 }
