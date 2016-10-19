47727e690278060fb116d753046de37706dca460
DevRaptor <krzysztoftaperek@gmail.com>
2016-10-19 09:13:48 +0200

    Added minigames changing system to Raptor.cs

--- Assets/Raptor.cs
+++ Assets/Raptor.cs
@@ -16,24 +16,29 @@ public class Raptor : MonoBehaviour {
 	    switch (currentAction)
         {
 		case Actions.Mister1:
-			GetComponent<Mister_one>().enabled = true;
+			if(GetComponent<Mister_one>().enabled == false) {
+				GetComponent<Mister_two>().enabled = true;
+				currentAction++;
+			}
 			break;
 		case Actions.Mister2:
-			GetComponent<Mister_one>().enabled = false;
-			GetComponent<Mister_two>().enabled = true;
+			if(GetComponent<Mister_two>().enabled == false) {
+				GetComponent<Mister_three>().enabled = true;
+				currentAction++;
+			}
 			break;
 		case Actions.Mister3:
 			/*GetComponent<Mister_two>().enabled = false;
 			GetComponent<Mister_three>().enabled = true;
 			*/
 			if(GetComponent<Mister_three>().enabled == false) {
+				GetComponent<Mister_four>().enabled = true;
 				currentAction++;
 			}
 
 			break;
 		case Actions.Mister4:
-			GetComponent<Mister_three> ().Stop ();
-			GetComponent<Mister_four>().enabled = true;
+			//GetComponent<Mister_four>().enabled = true;
 			break;
 		/*case Actions.Mister5:
 			GetComponent<Mister_four>().enabled = false;
