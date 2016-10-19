a205dc8635cf0bb7091b43d45aadf07fecf540c5
nietup <hhh>
2016-10-19 12:44:09 +0200

    girl 1 v1

---
+++ Assets/GirlOneMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GirlOneMovement : MonoBehaviour {
+
+    private float offset = 0.0f;
+    private Vector2 GirlStartPosition;
+    public float XMovementRange = 7.0f;
+    public float YMovementRange = 1.0f;
+    public Transform GameController;
+
+	// Use this for initialization
+	void Start () {
+        enabled = false;
+        GirlStartPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset += 0.01f;
+
+        Vector2 newPos = new Vector2(
+            XMovementRange * (float)Math.Sin(offset) + GirlStartPosition.x,
+            YMovementRange * (float)Math.Cos(7*offset) + GirlStartPosition.y
+        );
+
+        transform.position = newPos;
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "Missile")
+            GameController.GetComponent<Nietup>().currentAction++;
+    }
+}
--- Assets/Nietup.cs
+++ Assets/Nietup.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class Nietup : MonoBehaviour {
 
     public Actions currentAction;
+    //public Transform StoneGun;
+    public Transform GirlGhost;
 
 	// Use this for initialization
 	void Start () {
-        currentAction = Actions.Kids1;
+        currentAction = Actions.Mister4;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    switch (currentAction)
         {
-            case Actions.Kids1:
+            case Actions.Girl1:
+                GetComponent<GirlOne>().enabled = true;
+                GirlGhost.GetComponent<GirlOneMovement>().enabled = true;
+                break;
+            case Actions.Girl2:
+                GetComponent<GirlOne>().enabled = false;
+                GirlGhost.GetComponent<GirlOneMovement>().enabled = false;
                 break;
         }
 
