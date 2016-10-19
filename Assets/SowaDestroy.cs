427f4817358536d193c63cc56b4c361b1cb09d5f
DevRaptor <krzysztoftaperek@gmail.com>
2016-10-19 04:11:12 +0200

    Added destroying owl to parts

---
+++ Assets/SowaDestroy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SowaDestroy : MonoBehaviour {
+	public bool destroyed = false;
+	// Use this for initialization
+	void Start () {
+		destroyed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (destroyed) {
+			for (int i = 0; i < transform.childCount; i++) {
+				transform.GetChild (i).gameObject.transform.position += transform.GetChild (i).gameObject.transform.right * 1f * Time.deltaTime;
+			}
+		
+		}
+	}
+
+	public void DestroySowa() {
+		destroyed = true;
+
+		GetComponent<Rigidbody2D> ().gravityScale = 2.0f;
+
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).gameObject.transform.Rotate(0f, 0f, Random.Range(0f, 360f));
+		}
+	}
+}
