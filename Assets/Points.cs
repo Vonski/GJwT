a151c52893bbefb365055a2cc3ba2954721a58d6
Vonski <daniellodelapedro@interia.pl>
2016-10-20 02:30:58 +0200

    Working first art

---
+++ Assets/Points.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Points : MonoBehaviour {
+
+    public int ghost1, ghost2, ghost3;
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
--- Assets/RotateMissile.cs
+++ Assets/RotateMissile.cs
@@ -14,7 +14,19 @@ public class RotateMissile : MonoBehaviour {
     {
         if(tag=="Enemy_missile" || coll.gameObject.tag== "Enemy_missile")
             GetComponent<Rigidbody2D>().gravityScale = 2f;
-        if ((tag == "Enemy_missile" && coll.gameObject.tag == "Player") || (tag == "Player" && coll.gameObject.tag == "Enemy_missile"))
+        if (tag == "Enemy_missile" && coll.gameObject.tag == "Actual")
+        {
+            GameObject spawned;
+            if (coll.gameObject.transform.GetComponent<Rigidbody2D>().mass>20000)
+                spawned = (GameObject)Instantiate(Resources.Load("Prefabs/BoyFatFinal"), coll.gameObject.transform.position, coll.gameObject.transform.transform.rotation);
+            else
+                spawned = (GameObject)Instantiate(Resources.Load("Prefabs/GirlFatFinal"), coll.gameObject.transform.position, coll.gameObject.transform.transform.rotation);
+            spawned.transform.parent = coll.gameObject.transform.parent;
+            spawned.transform.localScale = coll.gameObject.transform.localScale;
+
+            Destroy(coll.gameObject);
+
             Debug.Log("Failure");
+        }
     }
 }
