52503a2db4dc5684e6a857f40c8eaeb281df43a4
nietup <hhh>
2016-10-20 11:30:10 +0200

    lerpienie w kosmos

--- Assets/GirlFour.cs
+++ Assets/GirlFour.cs
@@ -7,9 +7,12 @@ public class GirlFour : MonoBehaviour {
     bool FirstH = false;
     bool SecondH = false;
     GameObject Johny;
+    int shakes = 0;
+    public bool lerp = false;
+    Vector3 dest;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         
     }
 
@@ -20,26 +23,53 @@ public class GirlFour : MonoBehaviour {
             Johny = (GameObject)Instantiate(Resources.Load("Prefabs/Jonhy"), transform.parent.transform.position + new Vector3(1.0f, -3.6f, 0), Quaternion.Euler(0f, 0f, 95f));
             Johny.transform.localScale = new Vector3(0.5f, 0.5f, 0);
             Started = FirstH = true;
+            dest = transform.parent.transform.position + new Vector3(0, 72.0f, 0);
         }
 
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        if (!lerp) { 
         if (FirstH)
-        if (Input.GetKeyDown("up"))
-        {
+            if (Input.GetKeyDown("up"))
+            {
                 SecondHands();
-        }
+                shakes++;
+            }
 
         if (SecondH)
         {
             if (Input.GetKeyDown("down"))
             {
                 FirstHands();
+                shakes++;
             }
         }
+
+        if (shakes == 8)
+        {
+            lerp = true;
+        }
+    }
 	}
+
+    void FixedUpdate()
+    {
+        if (lerp)
+        {
+            Vector3 diff = dest - transform.parent.transform.position;
+            diff.Normalize();
+            if (Vector3.Distance(transform.parent.transform.position, dest) > 0.3f)
+            {
+
+                transform.parent.transform.position += diff * 0.4f;
+            }
+            //else
+                //GameController.GetComponent<ScenarioController>().currentAction++;
+        }
+    }
 
     void FirstHands()
     {
